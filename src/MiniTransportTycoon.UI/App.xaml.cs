24ae7bdca08c387d9766108b78a2f221efe61ad7
Gonter Mark <e86rbx@inf.elte.hu>
2026-03-18 11:48:56 +0100

    added usercontrols for menu and game

--- src/MiniTransportTycoon.UI/App.xaml.cs
+++ src/MiniTransportTycoon.UI/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Configuration;
+﻿using MiniTransportTycoon.UI.Views;
+using System.Configuration;
 using System.Data;
 using System.Windows;
 
@@ -9,6 +10,23 @@ namespace MiniTransportTycoon.UI
     /// </summary>
     public partial class App : Application
     {
+        private MainWindow _mainWindow = null!;
+        private GameView gameView = null!;
+        private MenuView menuView = null!;
+
+        public App()
+        {
+            Startup += OnStartup;
+        }
+
+        private void OnStartup(object sender, StartupEventArgs e)
+        {
+            gameView = new GameView();
+            menuView = new MenuView();
+            _mainWindow = new MainWindow();
+            _mainWindow.contentControl.Content = menuView;
+            _mainWindow.Show();
+        }
     }
 
 }
