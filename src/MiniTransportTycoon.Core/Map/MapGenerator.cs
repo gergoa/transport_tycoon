3782953931967beded5ccd3dc21c04e80c7f49d5
Laura Jager <iw3thz@inf.elte.hu>
2026-03-21 22:16:34 +0100

    random folyó generálása

--- src/MiniTransportTycoon.Core/Map/MapGenerator.cs
+++ src/MiniTransportTycoon.Core/Map/MapGenerator.cs
@@ -32,9 +32,40 @@ namespace MiniTransportTycoon.Core.Map
 
         private void GenerateTerrain(Field[,] board)
         {
-            double frequency = 0.07; //minnél kisebb annál jobban elkülönülnek a típusok
+            double frequency = 0.07; //minél kisebb annál jobban elkülönülnek a típusok
             double seedX = random.NextDouble() * 1000;
             double seedY = random.NextDouble() * 1000;
+
+            //folyó - egyik oldalról szembe - random irányban és kanyarokkal/vastagsággal
+            bool[,] isRiver = new bool[_width, _height];
+            bool isHorizontal = random.Next(0, 2) == 0;
+            int currX = isHorizontal ? 0 : random.Next(20, 80);
+            int currY = isHorizontal ? random.Next(20, 80) : 0;
+
+            while ((isHorizontal && currX < 100) || (!isHorizontal && currY < 100))
+            {
+                if (currX >= 0 && currX < 100 && currY >= 0 && currY < 100)
+                {
+                    isRiver[currX, currY] = true;
+                    if (isHorizontal && currY + 1 < 100) isRiver[currX, currY + 1] = true;
+                    if (!isHorizontal && currX + 1 < 100) isRiver[currX + 1, currY] = true;
+                }
+
+                int dir = random.Next(100);
+
+                if (isHorizontal)
+                {
+                    if (dir < 30) currX++;
+                    else if (dir < 65 && currY > 5) currY--;
+                    else if (dir < 100 && currY < 95) currY++;
+                }
+                else
+                {
+                    if (dir < 30) currY++;
+                    else if (dir < 65 && currX > 5) currX--;
+                    else if (dir < 100 && currX < 95) currX++;
+                }
+            }
 
             for (int x = 0; x < _width; x++)
             {
@@ -46,7 +77,7 @@ namespace MiniTransportTycoon.Core.Map
                     //int treeCount = 0;
 
                     // Küszöbértékek a Perlin zajhoz
-                    if (noise < 0.30)
+                    if ((noise < 0.25) || (isRiver[x,y]))
                     {
                         type = FieldType.WATER;
                     }
