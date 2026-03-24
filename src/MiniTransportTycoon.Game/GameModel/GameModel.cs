10943d14aa287060c344c9ff92bef2ae0d8f83cc
Laura Jager <iw3thz@inf.elte.hu>
2026-03-24 21:55:13 +0100

    Add road building logic with terrain-based costs

--- src/MiniTransportTycoon.Game/GameModel/GameModel.cs
+++ src/MiniTransportTycoon.Game/GameModel/GameModel.cs
@@ -62,7 +62,20 @@ namespace MiniTransportTycoon.Game.GameModel
 
         public void BuildRoad(Field field)
         {
+            int cost = 0;
 
+            if (field.Type == FieldType.EMPTY) { cost = 1; }
+            else if (field.Type == FieldType.FOREST) { cost = 2; }
+            else { return; }
+
+            if (EconomyManager.GetBalance() >= cost)
+            {
+                EconomyManager.SubtractMoney(cost);
+
+                field.Type = FieldType.ROAD;
+
+                FieldChanged?.Invoke(field.X, field.Y, FieldType.ROAD);
+            }
         }
 
         public void GameOver()
