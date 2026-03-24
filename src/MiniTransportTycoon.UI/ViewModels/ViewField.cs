735d801635c0247fab2358f0b3a622710e454839
Göntér Márk Dávid <e86rbx@inf.elte.hu>
2026-03-24 11:04:48 +0100

    Add ViewField class

---
+++ src/MiniTransportTycoon.UI/ViewModels/ViewField.cs
@@ -0,0 +1,37 @@
+﻿using MiniTransportTycoon.Core.Map;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniTransportTycoon.UI.ViewModels
+{
+    public class ViewField : ViewModelBase
+    {
+        private FieldType type;
+
+        public FieldType Type
+        {
+            get { return type; }
+            set
+            {
+                if (type != value)
+                {
+                    type = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public int X {  get; set; }
+        public int Y { get; set; }
+
+        public Tuple<int, int> XY
+        {
+            get { return new(X, Y); }
+        }
+
+        public DelegateCommand? FieldClickCommand { get; set; }
+    }
+}
