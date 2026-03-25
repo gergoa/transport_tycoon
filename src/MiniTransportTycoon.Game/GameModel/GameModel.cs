4b76a873ed5de4ce6fa15f9316eeff6c23515b25
Göntér Márk Dávid <e86rbx@inf.elte.hu>
2026-03-25 12:44:25 +0100

    Change grid size to 50x50

--- src/MiniTransportTycoon.Game/GameModel/GameModel.cs
+++ src/MiniTransportTycoon.Game/GameModel/GameModel.cs
@@ -11,8 +11,8 @@ namespace MiniTransportTycoon.Game.GameModel
 {
     public class GameModel
     {
-        private int width = 100;
-        private int height = 100;
+        private int width = 50;
+        private int height = 50;
         private Field[,] board = null!;
         private List<Facility> facilities = new List<Facility>();
         private TimeManager timeSystem = new TimeManager();
