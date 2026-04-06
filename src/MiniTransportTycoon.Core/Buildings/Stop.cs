11731ad50aeef3d8ec7e3ef7b1e1aae77a0e32fa
Göntér Márk Dávid <e86rbx@inf.elte.hu>
2026-04-06 15:17:51 +0200

    Add Route and Stop skeleton

---
+++ src/MiniTransportTycoon.Core/Buildings/Stop.cs
@@ -0,0 +1,21 @@
+using MiniTransportTycoon.Core.Cargo;
+using MiniTransportTycoon.Core.Vehicles;
+
+namespace MiniTransportTycoon.Core.Buildings
+{
+    public class Stop
+    {
+        private int waitingPassengers;
+        private Dictionary<CargoType, int> cargoStorage;
+
+        public void loadTo(Vehicle vehicle)
+        {
+
+        }
+
+        public void unloadFrom(Vehicle vehicle)
+        {
+
+        }
+    }
+}
---
+++ src/MiniTransportTycoon.Core/Routes/Route.cs
@@ -0,0 +1,28 @@
+using MiniTransportTycoon.Core.Buildings;
+using MiniTransportTycoon.Core.Map;
+using MiniTransportTycoon.Core.Pathfinding;
+
+namespace MiniTransportTycoon.Core.Routes
+{
+    public class Route
+    {
+        private List<Stop> stops;
+        private List<Field> path;
+        private bool loop;
+
+        public void addStop(Stop stop)
+        {
+
+        }
+
+        public void removeStop(Stop stop)
+        {
+
+        }
+
+        public void computePath(Pathfinder pathfinder)
+        {
+
+        }
+    }
+}
