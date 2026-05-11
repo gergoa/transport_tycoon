bf336041d2e48aacc24005aa4f21baa8d7ae4aa4
Göntér Márk Dávid <e86rbx@inf.elte.hu>
2026-05-12 00:27:29 +0200

    Add tier 2 and tier 3 cargotypes to city demand

--- src/MiniTransportTycoon.Game/GameModel/FacilityManager.cs
+++ src/MiniTransportTycoon.Game/GameModel/FacilityManager.cs
@@ -219,11 +219,31 @@ namespace MiniTransportTycoon.Game.GameModel
 
             facilities.Add(new City(CreateCityBlock(FindEmptyFields(model)), 2000, "Metropolis")
             {
-                Demand = new Dictionary<CargoType, int> { { CargoType.Passengers, 1} }
+                Demand = new Dictionary<CargoType, int> { 
+                    { CargoType.Passengers, 1},
+                    //TIER 2
+                    { CargoType.Microchips, 1 },
+                    { CargoType.Tools, 1 },
+                    { CargoType.Furniture, 1 },
+                    { CargoType.ProcessedFood, 1 },
+                    //TIER 3
+                    { CargoType.Electronics, 1 },
+                    { CargoType.Automobiles, 1 }
+                }
             });
             facilities.Add(new City(CreateCityBlock(FindEmptyFields(model)), 800, "Smallville")
             {
-                Demand = new Dictionary<CargoType, int> { { CargoType.Passengers, 1 } }
+                Demand = new Dictionary<CargoType, int> {
+                    { CargoType.Passengers, 1},
+                    //TIER 2
+                    { CargoType.Microchips, 1 },
+                    { CargoType.Tools, 1 },
+                    { CargoType.Furniture, 1 },
+                    { CargoType.ProcessedFood, 1 },
+                    //TIER 3
+                    { CargoType.Electronics, 1 },
+                    { CargoType.Automobiles, 1 }
+                }
             });
 
             // TIER 0 - raw producers, no input needed
